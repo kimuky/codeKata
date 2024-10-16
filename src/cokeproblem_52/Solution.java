c7f98587d9e5fbc072fbec98f33bfef6d9d092b7
kimuky <xogus@gmail.com>
2024-10-16 09:47:41 +0900

    52번 콜라 문제

---
+++ src/cokeproblem_52/Solution.java
@@ -0,0 +1,21 @@
+package cokeproblem_52;
+
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int emptyBottle;
+        int remainBottle;
+
+        while(true) {
+            if (n< a){
+                break;
+            }
+
+            emptyBottle = b*(n/a);
+            remainBottle = n%a;
+            answer += emptyBottle;
+            n = emptyBottle + remainBottle;
+        }
+        return answer;
+    }
+}
