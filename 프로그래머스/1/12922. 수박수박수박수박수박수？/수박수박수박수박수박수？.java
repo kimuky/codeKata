1875e80e471d4c7a5e8e44ffd8eebeee75444ed0
kimuky <119008728+kimuky@users.noreply.github.com>
2024-10-18 09:55:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.83 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        if (n == 1) {
+            return "수";
+        }
+        
+        if (n%2==0) {
+            answer = "수박".repeat(n/2);
+        } else {
+            answer = "수박".repeat(n/2) + "수";
+        }
+        
+        return answer;
+    }
+}
