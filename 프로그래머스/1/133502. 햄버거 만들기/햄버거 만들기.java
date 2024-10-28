0018acc170188b3c9d02ac53e78f1173a4470cf8
kimuky <119008728+kimuky@users.noreply.github.com>
2024-10-29 00:13:25 +0900

    [level 1] Title: 햄버거 만들기, Time: 72.71 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i : ingredient) {
+            
+            if (stack.size() >= 3) {
+                
+                if (i== 1) {
+                    int meat = stack.pop();
+                    int veg = stack.pop();
+                    int bread = stack.pop();
+                    if (meat == 3 && veg == 2 && bread == 1) {
+                        answer +=1;
+                    } else {
+                        stack.push(bread);
+                        stack.push(veg);
+                        stack.push(meat);
+                        stack.push(i);
+                    }
+                } else {
+                    stack.push(i);    
+                }
+            } else {
+                stack.push(i);  
+            }
+            
+        }
+        
+        return answer;
+    }
+}
