18186b01551b402ed52ec0a7bb372673f78e841a
Tae Hyun Kim <119008728+kimuky@users.noreply.github.com>
2024-11-18 09:54:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 1454.31 ms, Memory: 174 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        int[] reportedArr = new int[id_list.length];
+        
+        Set<String> reportSet = new TreeSet<>(Arrays.asList(report));
+        
+        for (String reportString : reportSet) {
+            String[] reportTwo = reportString.split(" ");
+
+            String reporter = reportTwo[0];
+            String reported = reportTwo[1];
+               
+            if(Arrays.asList(id_list).indexOf(reported)>-1) {
+                reportedArr[Arrays.asList(id_list).indexOf(reported)] +=1;
+            }    
+        }
+        
+        System.out.println(Arrays.toString(reportedArr));
+        
+        for (String reportString : reportSet) {
+            String[] reportTwo = reportString.split(" ");
+
+            String reporter = reportTwo[0];
+            String reported = reportTwo[1];
+               
+        
+            if (reportedArr[Arrays.asList(id_list).indexOf(reported)] >=k) {
+                answer[Arrays.asList(id_list).indexOf(reporter)] +=1;
+            }   
+        }
+        
+        
+        return answer;
+    }
+}
